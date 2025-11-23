d4017c8fcbb713a8f09a7b5e36a0c5fdf78ca598
Wilberth-Lopez <wilberth_sl1@tesch.edu.mx>
2025-11-22 19:32:56 -0600

    Corregir dependencias y llaves en build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -41,12 +41,11 @@ dependencies {
     implementation(libs.androidx.appcompat)
     implementation(libs.material)
     implementation(libs.androidx.activity)
+    implementation(libs.androidx.constraintlayout)
 
     implementation("com.google.android.gms:play-services-maps:18.2.0")
     implementation("com.google.android.gms:play-services-location:21.2.0")
-}
 
-    implementation(libs.androidx.constraintlayout)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
